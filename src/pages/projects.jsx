6469eb9ae9a0c5b14b338d6c5f7d92e784d6aab9
Elvis Iraguha <iraguhaelvis@gmail.com>
2021-03-24 01:14:33 +0200

    fix the favicon image

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Grid, Box, GridItem } from '@chakra-ui/react';
+import { Grid, Box, GridItem, Text } from '@chakra-ui/react';
 import Sidebar from '../components/Sidebar';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Projects = () => {
   const projects = [
     {
-      title: 'tt',
-      link: 'll',
-      description: 'dd',
-      thumbnail: 'tt',
+      title: 'Ishuri',
+      link: 'ishuri.surge.sh',
+      description: 'lorem ipsum lrrr',
+      thumbnail: '../images/ishuri.png',
     },
     {
       title: 'Barefoot Nomad',
       link: 'http://script-heroes-bn-frontend-stg.herokuapp.com/',
       description: 'dd',
-      thumbnail: 'tt',
+      thumbnail: '../images/barefoot.png',
     },
     {
       title: 'tt',
@@ -58,7 +59,12 @@ const Projects = () => {
       <Box height="100%">
         {/* <Grid gridTemplateColumns="1fr 1fr 1fr" gridGap="20px">
           {projects.map((project, index) => (
-            <GridItem key={index}>project</GridItem>
+            <GridItem key={index}>
+              <Box>
+                <StaticImage src={'../images/ishuri.png'} alt={project.description} />
+                <Text>{project.title}</Text>
+              </Box>
+            </GridItem>
           ))}
         </Grid> */}
       </Box>
